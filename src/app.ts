86548062911ae99df50fa4adae3d0d4917379749
Jadack <jackdamiolixp@gmail.com>
2021-09-16 22:01:39 -0400

    Tslint and prettierr added to the project

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
 export class VowelsCounter {
-
   vowelsList: string[];
   vowelsQuantity: any;
-  constructor() { 
+  constructor() {
     this.vowelsList = ['A', 'E', 'I', 'O', 'U'];
     this.vowelsQuantity = {
       A: 0,
       E: 0,
       I: 0,
       O: 0,
-      U: 0
-    };   
+      U: 0,
+    };
   }
-  
+
   inputValidator(text: string) {
     const _text = typeof text === 'string' && text.length === 0 ? 'Entrada Invalida' : true;
     return _text;
@@ -21,10 +20,10 @@ export class VowelsCounter {
   vowelsCounter(text: string) {
     const _text = this.inputValidator(text);
     text = text.toUpperCase();
-    if(_text) {
-      for(let i = 0; i < text.length; i++) {
-        if(this.vowelsList.includes(text[i])) {
-          this.vowelsQuantity[text[i]] += 1;
+    if (_text) {
+      for (const letter of text) {
+        if (this.vowelsList.includes(letter)) {
+          this.vowelsQuantity[letter] += 1;
         }
       }
       return this.vowelsQuantity;
